0bf0a8985c94e0c48e4466bfdf6de08865e9fd75
Yel0w <144521481+Yel0w08@users.noreply.github.com>
2026-09-02 16:25:55 +0200

    Update installApps.sh to install additional packages

--- installApps.sh
+++ installApps.sh
@@ -1,18 +1,47 @@
+echo "Installing Brave..."
 curl -fsS https://dl.brave.com/install.sh | sh
+
+echo "Installing VLC..."
 sudo pacman -S vlc
+
+echo "Installing Git..."
 sudo pacman -S git
-sudo pacman -S kitty   
+
+echo "Installing Kitty..."
+sudo pacman -S kitty
+
+echo "Installing Git and base-devel..."
 sudo pacman -S --needed git base-devel
+
+echo "Installing Fastfetch..."
 sudo pacman -S fastfetch
+
+echo "Installing Tree..."
 sudo pacman -S tree
+
+echo "Installing 7zip..."
 sudo pacman -S 7zip
+
+echo "Installing Flatpak..."
 sudo pacman -S flatpak
+
+echo "Installing Pamac GTK..."
 sudo pacman -S pamac-gtk
+
+echo "Installing Flathub..."
 sudo flatpak install flathub
+
+echo "Installing Flathub..."
 flatpak install flathub
+
+echo "Installing Steam..."
 flatpak install flathub com.valvesoftware.Steam
 
-
+echo "Cloning yay..."
 git clone https://aur.archlinux.org/yay.git
+
+echo "Entering yay directory..."
 cd yay
-makepkg -si   
+
+echo "Building yay..."
+makepkg -si
